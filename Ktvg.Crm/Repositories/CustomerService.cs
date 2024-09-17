de5d128407ce7fc0f5a54ff6af2910e70c01bc68
Khanh Le <khanh.lenhut@hdwebsoft.dev>
2024-09-17 18:03:56 +0700

    update 3

---
+++ Ktvg.Crm/Repositories/CustomerService.cs
@@ -0,0 +1,48 @@
+﻿using Ktvg.Crm.Models;
+using Ktvg.Crm.Repositories.Interfaces;
+using Ktvg.Crm.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ktvg.Crm.Repositories
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly KtvgCrmContext _context;
+
+        public CustomerService(KtvgCrmContext context)
+        {
+            _context = context;
+        }
+
+
+        public Task<List<CustomerVM>> GetAllCustomersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> CreateCustomerAsync(CustomerVM model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> UpdateCustomerAsync(CustomerVM model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCustomerAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetNextCustomerCodeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ktvg.Crm/Repositories/Interfaces/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using Ktvg.Crm.Models;
+using Ktvg.Crm.ViewModels;
+
+namespace Ktvg.Crm.Repositories.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<List<CustomerVM>> GetAllCustomersAsync();
+        Task<Customer> GetCustomerByIdAsync(int id);
+        Task<Customer> CreateCustomerAsync(CustomerVM model);
+        Task<Customer> UpdateCustomerAsync(CustomerVM model);
+        Task DeleteCustomerAsync(int id);
+        Task<string> GetNextCustomerCodeAsync();
+    }
+}
